b311465044336b665ddbb042f77b01a4ab5a0255
lqrby <748862180@qq.com>
2018-02-24 18:06:04 +0800

    自定义组件

--- myvue/src/components/user/userList.vue
+++ myvue/src/components/user/userList.vue
@@ -1,55 +1,6 @@
 <template>
    <el-container style="border: 1px solid #eee">
-  <el-aside width="300px" style="background-color: rgb(238, 241, 246)">
-    <el-menu :default-openeds="['1', '3']">
-      <el-submenu index="1">
-        <template slot="title"><i class="el-icon-message"></i>导航一</template>
-        <el-menu-item-group>
-          <template slot="title">分组一</template>
-          <router-link :to="{path:'/direct'}"><el-menu-item index="1-1">选项1(自定义指令)</el-menu-item></router-link>
-          <el-menu-item index="1-2">选项2</el-menu-item>
-        </el-menu-item-group>
-        <el-menu-item-group title="分组2">
-          <el-menu-item index="1-3">选项3</el-menu-item>
-        </el-menu-item-group>
-        <el-submenu index="1-4">
-          <template slot="title">选项4</template>
-          <el-menu-item index="1-4-1">选项4-1</el-menu-item>
-        </el-submenu>
-      </el-submenu>
-      <el-submenu index="2">
-        <template slot="title"><i class="el-icon-menu"></i>导航二</template>
-        <el-menu-item-group>
-          <template slot="title">分组一</template>
-          <el-menu-item index="2-1">选项1</el-menu-item>
-          <el-menu-item index="2-2">选项2</el-menu-item>
-        </el-menu-item-group>
-        <el-menu-item-group title="分组2">
-          <el-menu-item index="2-3">选项3</el-menu-item>
-        </el-menu-item-group>
-        <el-submenu index="2-4">
-          <template slot="title">选项4</template>
-          <el-menu-item index="2-4-1">选项4-1</el-menu-item>
-        </el-submenu>
-      </el-submenu>
-      <el-submenu index="3">
-        <template slot="title"><i class="el-icon-setting"></i>导航三</template>
-        <el-menu-item-group>
-          <template slot="title">分组一</template>
-          <el-menu-item index="3-1">选项1</el-menu-item>
-          <el-menu-item index="3-2">选项2</el-menu-item>
-        </el-menu-item-group>
-        <el-menu-item-group title="分组2">
-          <el-menu-item index="3-3">选项3</el-menu-item>
-        </el-menu-item-group>
-        <el-submenu index="3-4">
-          <template slot="title">选项4</template>
-          <el-menu-item index="3-4-1">选项4-1</el-menu-item>
-        </el-submenu>
-      </el-submenu>
-    </el-menu>
-  </el-aside>
-  
+  <left></left>
   <el-container>
     <el-header style="text-align: right; font-size: 12px">
       <el-dropdown>
@@ -64,24 +15,14 @@
           <span v-text="LoginedUser.name"></span>
     </el-header>
     
-    <el-main>
-      <router-link :to="{name:'detail'}">
-        <el-table :data="tableData">
-          <el-table-column prop="userId" label="ID" width=""></el-table-column>
-          <el-table-column prop="date" label="日期" width=""></el-table-column>
-          <el-table-column prop="name" label="姓名" width=""></el-table-column>
-          <el-table-column prop="gender" label="性别"></el-table-column>
-          <el-table-column prop="age" label="年龄"></el-table-column>
-          <el-table-column prop="address" label="地址"></el-table-column>
-        </el-table>
-      </router-link>
-    </el-main>
+    <!-- <right></right> -->
   </el-container>
 </el-container>
 </template>
 
 <script>
-  import Vue from 'vue'
+  
+import Vue from 'vue'
   import store from '@/vuex/store';
   import { mapState,mapMutations,mapGetters,mapActions } from 'vuex';
   import axios from 'axios'
@@ -116,6 +57,8 @@
     },
     store
   };
+  import {left} from '@/components/globalComponents/left.js';
+  // import {right} from '@/components/globalComponents/right.js';
 </script>
 <style>
   .el-header {
